88da8778bd531c65ed13cf1659334a28706d1ad5
ohljp6 <33756663+ohljp6@users.noreply.github.com>
2017-11-17 10:12:22 -0500

    First Upload

---
+++ Personality_JO.py
@@ -0,0 +1,17 @@
+name = "JP"
+state = "California"
+city = "rome"
+game = "BF1"
+book = "Refugee"
+print(name + " likes to visit " + state + " and " + city)
+print(name + "'s favorite game is " + game + " and reads " + book)
+
+
+
+print("whats your favorite subject?")
+subject = input ()
+if subject == "Math":
+    print("Thats crazy me too!")
+else:
+    print(subject + " is alright I guess.")
+    
